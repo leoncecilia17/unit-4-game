582d71e665669323c8e6df5ebffa7f6c9c8957f9
Cecilia Leon <leoncecilia17@gmail.com>
2019-05-18 13:10:43 -0500

    starting jquery

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    var targetNumber = Math.ceil(Math.random() * 101) + 19;
+    $(".randomnumber").html(targetNumber);
+
+
+    var winscounter = 0; 
+    var losescounter = 0;
+    var sum = 0; 
+    var berryArr = ["./assets/images/blackberry.jpg", "./assets/images/blueberry.jpg", "./assets/images/raspberry.jpg", "./assets/images/strawberry.png"]
+    var berryrandomvalue = [ Math.ceil(Math.random() *   12) 
+
+})
+
