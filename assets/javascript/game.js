0f9baa64c4bdb5f961d79148c52f2bc576789c5e
Cecilia Leon <leoncecilia17@gmail.com>
2019-05-18 15:45:26 -0500

    jquery adding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,48 @@
 $(document).ready(function () {
 
-    var targetNumber = Math.ceil(Math.random() * 101) + 19;
-    $(".randomnumber").html(targetNumber);
+    var targetNumber 
+    var winscounter = 0;
+    var lossescounter = 0;
+    var sum = 0;
+    var totalstore = 0;
+
+    function reset(){
+        targetNumber = Math.ceil(Math.random() * 101) + 19;
+     $(".randomnumber").html(targetNumber); 
+        
+        totalstore = 0;    
+        $(".total").text(totalstore)
+        for (var i = 0; i < 4; i++) {
+            var berryrandomvalue = Math.ceil(Math.random() * 12)
+            $("#" + i).data("number", berryrandomvalue) 
+        }
+    }
+
+    reset()
+    $(".fruits").click(function () {
+        totalstore = totalstore + parseInt ($(this).data("number")); 
+        $(".total").text(totalstore);
+
+        if (totalstore === targetNumber) { 
+         winscounter++ 
+         $("#wins").text(winscounter)
+         $("#results").text("You won!") 
+         setTimeout(function(){
+             reset()
+         }, 2000)
+        }
+
+        else if (totalstore > targetNumber) {
+        lossescounter++ 
+        $("#losses").text(lossescounter)
+        $("#results").text("You lost!") 
+        setTimeout(function(){
+            reset()
+        }, 2000)
+        }
+
+    })
 
 
-    var winscounter = 0; 
-    var losescounter = 0;
-    var sum = 0; 
-    var berryArr = ["./assets/images/blackberry.jpg", "./assets/images/blueberry.jpg", "./assets/images/raspberry.jpg", "./assets/images/strawberry.png"]
-    var berryrandomvalue = [ Math.ceil(Math.random() *   12) 
 
 })
-
