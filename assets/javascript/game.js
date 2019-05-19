99cdb4db9593e3604e400e71d40e34df12ac1b6b
Cecilia Leon <leoncecilia17@gmail.com>
2019-05-18 21:56:32 -0500

    final css changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,44 @@
 $(document).ready(function () {
 
-    var targetNumber 
+    var targetNumber
     var winscounter = 0;
     var lossescounter = 0;
-    var sum = 0;
     var totalstore = 0;
 
-    function reset(){
+    function reset() {
         targetNumber = Math.ceil(Math.random() * 101) + 19;
-     $(".randomnumber").html(targetNumber); 
-        
-        totalstore = 0;    
+        $(".randomnumber").html(targetNumber);
+
+        totalstore = 0;
         $(".total").text(totalstore)
         for (var i = 0; i < 4; i++) {
-            var berryrandomvalue = Math.ceil(Math.random() * 12); 
-            $("#" + i).data("number", berryrandomvalue) 
+            var berryrandomvalue = Math.ceil(Math.random() * 12);
+            $("#" + i).data("number", berryrandomvalue)
         }
     }
 
     reset()
     $(".fruits").click(function () {
-        totalstore = totalstore + parseInt ($(this).data("number")); 
+        totalstore = totalstore + parseInt($(this).data("number"));
         $(".total").text(totalstore);
 
-        if (totalstore === targetNumber) { 
-         winscounter++ 
-         $("#wins").text(winscounter)
-         $("#results").text("You won!") 
-         setTimeout(function(){
-             reset()
-         }, 2000)
+        if (totalstore === targetNumber) {
+            winscounter++
+            $("#wins").text(winscounter)
+            $("#results").text("You won!")
+            setTimeout(function () {
+                reset()
+            }, 2000)
         }
 
         else if (totalstore > targetNumber) {
-        lossescounter++ 
-        $("#losses").text(lossescounter)
-        $("#results").text("You lost!") 
-        setTimeout(function(){
-            reset()
-        }, 2000)
+            lossescounter++
+            $("#losses").text(lossescounter)
+            $("#results").text("You lost!")
+            setTimeout(function () {
+                reset()
+            }, 2000)
         }
 
     })
-
-
-
 })
